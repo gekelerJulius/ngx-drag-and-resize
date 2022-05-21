d0019dfafcd3c3f517e1e1a79a2eae1e4b234ccf
Julius Gekeler <julius.gekeler@cytolytics.de>
2022-05-21 22:33:13 +0200

    remove unused code

--- projects/ngx-drag-and-resize/src/lib/draggable-element.directive.ts
+++ projects/ngx-drag-and-resize/src/lib/draggable-element.directive.ts
@@ -1,10 +1,9 @@
-import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[draggableElement]',
 })
 export class DraggableElementDirective {
-  @Input() millisBetweenUpdates = 1;
   private dragging = false;
   private mousePosition: { x: number; y: number } = { x: 0, y: 0 };
 
