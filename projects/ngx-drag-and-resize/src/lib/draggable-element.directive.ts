ad9264e131eefe3c00e239a386dd64eb1a76a805
Julius Gekeler <julius.gekeler@cytolytics.de>
2022-05-30 21:59:27 +0200

    0.1.0

--- projects/ngx-drag-and-resize/src/lib/draggable-element.directive.ts
+++ projects/ngx-drag-and-resize/src/lib/draggable-element.directive.ts
@@ -1,15 +1,17 @@
-import {Directive, ElementRef, EventEmitter, HostListener, Output, Renderer2} from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from "@angular/core";
 
 @Directive({
   selector: '[draggableElement]',
 })
 export class DraggableElementDirective {
-  private dragging = false;
-  private mousePosition: { x: number; y: number } = { x: 0, y: 0 };
+  @Input() ngxBoundsContainerSelector: string | null = null;
 
   @Output() ngxDragStart: EventEmitter<void> = new EventEmitter<void>();
   @Output() ngxDragging: EventEmitter<void> = new EventEmitter<void>();
   @Output() ngxDragEnd: EventEmitter<void> = new EventEmitter<void>();
+
+  private dragging = false;
+  private mousePosition: { x: number; y: number } = { x: 0, y: 0 };
 
   constructor(private elRef: ElementRef<HTMLElement>, private renderer2: Renderer2) {
     if (this.elRef.nativeElement.parentElement) {
@@ -39,18 +41,51 @@ export class DraggableElementDirective {
   @HostListener('window:mousemove', ['$event']) onMouseMove(ev: MouseEvent) {
     if (this.dragging && this.mousePosition && !ev.defaultPrevented && ev.returnValue) {
       ev.preventDefault();
-      const xDiff = ev.clientX - this.mousePosition.x;
-      const yDiff = ev.clientY - this.mousePosition.y;
-      const style = window.getComputedStyle(this.elRef.nativeElement);
-      const matrix = new WebKitCSSMatrix(style.transform);
-      const oldX = matrix.m41;
-      const oldY = matrix.m42;
+      let xDiff = ev.clientX - this.mousePosition.x;
+      let yDiff = ev.clientY - this.mousePosition.y;
+      let style = window.getComputedStyle(this.elRef.nativeElement);
+      let matrix = new WebKitCSSMatrix(style.transform);
+      let oldX = matrix.m41;
+      let oldY = matrix.m42;
+      let rect = this.elRef?.nativeElement?.getBoundingClientRect();
+      const boundsContainer =
+        typeof this.ngxBoundsContainerSelector === 'string'
+          ? document.querySelector(this.ngxBoundsContainerSelector)
+          : null;
 
+      if (boundsContainer) {
+        if (oldX + xDiff < boundsContainer.getBoundingClientRect().x) {
+          xDiff = boundsContainer.getBoundingClientRect().x - oldX;
+        }
+        if (
+          oldX + xDiff + rect.width >
+          boundsContainer.getBoundingClientRect().x + boundsContainer.getBoundingClientRect().width
+        ) {
+          xDiff =
+            boundsContainer.getBoundingClientRect().x +
+            boundsContainer.getBoundingClientRect().width -
+            (oldX + rect.width);
+        }
+
+        if (oldY + yDiff < boundsContainer.getBoundingClientRect().y) {
+          yDiff = boundsContainer.getBoundingClientRect().y - oldY;
+        }
+        if (
+          oldY + yDiff + rect.height >
+          boundsContainer.getBoundingClientRect().y + boundsContainer.getBoundingClientRect().height
+        ) {
+          yDiff =
+            boundsContainer.getBoundingClientRect().y +
+            boundsContainer.getBoundingClientRect().height -
+            (oldY + rect.height);
+        }
+      }
       this.renderer2.setStyle(
         this.elRef.nativeElement,
         'transform',
         this.asTranslateString(oldX + xDiff, oldY + yDiff)
       );
+
       this.ngxDragging.emit();
     }
     this.mousePosition = { x: ev.clientX, y: ev.clientY };
